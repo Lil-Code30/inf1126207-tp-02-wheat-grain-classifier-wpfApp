0b48cd317c3d0354f5ba8bf8e20044b81fdc016b
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-05-06 15:33:22 -0400

    feat: Add API service, User model & DbContext
    Introduce ApiService to fetch users from https://dummyjson.com (GetUsersAsync and a sync wrapper), add User model with JSON property mappings and FullName helper, and add a placeholder WheatGrainClassifierDbContext. Also update MainWindow.xaml label from "Expériences" to "Historique" and add a Newtonsoft.Json package reference to the project file.

---
+++ Db/WheatGrainClassifierDbContext.cs
@@ -0,0 +1,9 @@
+﻿using WheatGrainClassifierWpfApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WheatGrainClassifierWpfApp.Db
+{
+    public class WheatGrainClassifierDbContext : DbContext
+    {
+    }
+}
---
+++ Services/ApiService.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System.Buffers.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WheatGrainClassifierWpfApp.Models;
+
+namespace WheatGrainClassifierWpfApp.Services
+{
+    public class ApiService
+    {
+        private HttpClient _httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://dummyjson.com/");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        public async Task<List<User>> GetUsersAsync(string path, int limit = 10)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"{path}?limit={limit}");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                string data = await response.Content.ReadAsStringAsync();
+                List<User> users = JsonConvert.DeserializeObject<List<User>>(data);
+                return users;
+            }
+
+            return new List<User>();
+        }
+
+        public List<User> GetUsers(string path)
+        {
+            List<User> users = new List<User>();
+
+            try
+            {
+                Task<List<User>> task = Task.Run(async () => await GetUsersAsync(path));
+                task.Wait();
+                users = task.Result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return users;
+        }
+
+
+    }
+}
